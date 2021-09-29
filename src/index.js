2499bebc1ba5da2440ea6bac1f09499b7a8e46b4
AZARIA <allysaidi64@gmail.com>
2021-09-29 11:16:56 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import _ from 'lodash';
 import './css/style.css';
 
 const component = () => {
-    const element = document.createElement('p');
-    element.textContent= _.join(["Hello"," Webpack By Azy"]," ");
-    return element;
-}
+  const element = document.createElement('p');
+  element.textContent = _.join(['Hello', ' Webpack By Azy'], ' ');
+  return element;
+};
 
 document.body.append(component());
--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'tunaweza.js',
-        clean: true,
-    },
-    mode: 'development',
-    devtool: 'inline-source-map',
-    devServer: {
-        static:'/dist'
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'toDoList',
-            template: './src/index.html',
-        })
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'tunaweza.js',
+    clean: true,
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: '/dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'toDoList',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+      },
     ],
-    module: {
-        rules:[
-            {
-                use :['style-loader','css-loader'],
-                test: /\.css$/i,
-            }
-        ]
-    }
+  },
 
-}
+};
