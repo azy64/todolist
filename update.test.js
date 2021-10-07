a15ebe56ff816da1df6acc0bf274d00a856db5a8
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-10-07 11:52:16 +0200

    Create file update.test.js; Add test for changeStatus function

---
+++ update.test.js
@@ -0,0 +1,46 @@
+/**
+ * @jest-environment jsdom
+ */
+import Update from './src/update.js';
+
+// Arrange
+const innerHtml = `
+<div id="task-container">
+  <input id="2" type="checkbox" checked="true"/>
+</div>`;
+document.body.innerHTML = innerHtml;
+
+const changedStateMocked = jest.fn((data = []) => {
+  const checkboxs = document.querySelectorAll('#task-container input[type="checkbox"]');
+  checkboxs.forEach((checkbox) => {
+    const id = parseInt(checkbox.id, 10);
+    const num = data.findIndex((value) => value.index === id);
+    if (checkbox.checked) {
+      data[num].completed = true;
+    } else {
+      data[num].completed = false;
+    }
+  });
+  return data;
+});
+
+describe('#changedState() is working properly', () => {
+  test('changes the completed state if input', () => {
+    // Arrange
+    const tempData = [
+      { index: 1, description: 'Task 1', completed: false },
+      { index: 2, description: 'Task 2', completed: false },
+      { index: 3, description: 'Task 3', completed: false },
+      { index: 4, description: 'Task 4', completed: false },
+    ];
+
+    const expectedRes = [
+      { index: 1, description: 'Task 1', completed: false },
+      { index: 2, description: 'Task 2', completed: true },
+      { index: 3, description: 'Task 3', completed: false },
+      { index: 4, description: 'Task 4', completed: false },
+    ];
+    // Assess
+    expect(changedStateMocked(tempData)).toEqual(expectedRes);
+  });
+});
